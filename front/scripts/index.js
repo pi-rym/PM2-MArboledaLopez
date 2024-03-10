8f88e324873123acd3fc2ee256eef6b0539fd8d6
HonguiperMA <lanhonguiper@gmail.com>
2024-03-09 21:55:01 -0500

    tarea de clase 1 terminada por completo, pagina sobre mi sin css. Lista para lo otro

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,1 +1,28 @@
-console.log(tempData);
+document.addEventListener("DOMContentLoaded", function () {
+
+    const filmContainer = document.getElementById("filmcontainer");
+    tempData.forEach((movie) => {
+      const movieCard = document.createElement("div");
+      movieCard.classList.add("tarjeta");
+  
+      //HTML contenedor.
+      movieCard.innerHTML = `
+        <div class="cajaimagen">
+          <img src="${movie.poster}" alt="${movie.title}">
+            <div class="contenidos">
+                <div>
+                    <h2>${movie.title}</h2>
+                    <p>Year: ${movie.year}</p>
+                    <p>Director: ${movie.director}</p>
+                    <p>Duration: ${movie.duration}</p>
+                    <p>Genre: ${movie.genre.join(", ")}</p>
+                    <p>Rate: ${movie.rate}</p>
+                </div>
+            </div>
+        </div>
+      `;
+  
+      filmContainer.appendChild(movieCard);
+    });
+});
+  
