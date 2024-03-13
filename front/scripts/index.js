d5fb46ff5c50da0c48dd8ca8f4daf2c8882d5964
HonguiperMA <lanhonguiper@gmail.com>
2024-03-12 23:40:25 -0500

    tarea clase 2 modulo 2 realizada, pero no se puede hacer por un bloqueo de cors de parte del servidor al parecer. Mas de eso en los comentarios del commit

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,28 +1,42 @@
-document.addEventListener("DOMContentLoaded", function () {
+$(document).ready(function() {
+  const filmContainer = $("#filmcontainer");
 
-    const filmContainer = document.getElementById("filmcontainer");
-    tempData.forEach((movie) => {
-      const movieCard = document.createElement("div");
-      movieCard.classList.add("tarjeta");
   
-      //HTML contenedor.
-      movieCard.innerHTML = `
-        <div class="cajaimagen">
-          <img src="${movie.poster}" alt="${movie.title}">
-            <div class="contenidos">
-                <div>
-                    <h2>${movie.title}</h2>
-                    <p>Year: ${movie.year}</p>
-                    <p>Director: ${movie.director}</p>
-                    <p>Duration: ${movie.duration}</p>
-                    <p>Genre: ${movie.genre.join(", ")}</p>
-                    <p>Rate: ${movie.rate}</p>
-                </div>
-            </div>
-        </div>
-      `;
-  
-      filmContainer.appendChild(movieCard);
-    });
+  $.get("https://students-api.2.us-1.fl0.io/movies", function(data) {
+      
+      if (Array.isArray(data)) {
+          
+          data.forEach(function(movie) {
+              
+              const movieCard = $("<div>").addClass("tarjeta");
+
+              
+              const htmlContent = `
+                  <div class="cajaimagen">
+                      <img src="${movie.poster}" alt="${movie.title}">
+                      <div class="contenidos">
+                          <div>
+                              <h2>${movie.title}</h2>
+                              <p>Year: ${movie.year}</p>
+                              <p>Director: ${movie.director}</p>
+                              <p>Duration: ${movie.duration}</p>
+                              <p>Genre: ${movie.genre.join(", ")}</p>
+                              <p>Rate: ${movie.rate}</p>
+                          </div>
+                      </div>
+                  </div>
+              `;
+
+              
+              movieCard.html(htmlContent);
+
+              
+              filmContainer.append(movieCard);
+          });
+      } else {
+          console.error("No es un array :/.");
+      }
+  }).fail(function() {
+      console.error("No se pudo acceder a los datos de las peliculas.");
+  });
 });
-  
