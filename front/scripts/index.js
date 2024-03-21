132750b05b80c8f5f634cca626639c52908518f9
HonguiperMA <lanhonguiper@gmail.com>
2024-03-21 01:05:34 -0500

    finalmente PROYECTO M2 FINALIZADO

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -4,7 +4,7 @@ import { fetchData } from './dataFetch.js';
 import { renderMovies } from './renderCards.js';
 
 $(document).ready(function() {
-  const apiUrl = "http://localhost:3000";
+  const apiUrl = "http://localhost:3000/Movies";
 
   fetchData(apiUrl, renderMovies, console.error);
 });
